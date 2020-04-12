3c8502a796411923042cd08ffb044083320eaa03
Saeed Shahini <saeedshahiniit@gmail.com>
2020-04-12 06:19:13 +0330

    first commit

---
+++ app/src/main/java/com/sevenlearn/filemanager/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sevenlearn.filemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Environment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
